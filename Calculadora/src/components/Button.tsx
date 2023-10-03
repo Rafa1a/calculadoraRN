9fe3f1163c4376cb488af6ef421403b07fa14d03
Rafael Leal Altero <rafa.altero@gmail.com>
2023-10-03 15:37:21 -0300

    up

---
+++ Calculadora/src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React, {useState,useEffect,useReducer} from "react";
+import { 
+    Text, StyleSheet, SafeAreaView, View, Button, Dimensions, TouchableHighlight
+
+ } from "react-native";
+
+interface CompProps {
+
+};
+
+const exemplo: React.FC<any> = (props) => {
+    return (
+        <>
+        <Text></Text>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    button:{
+        fontSize:40,
+    }
+
+});
+export default exemplo;
